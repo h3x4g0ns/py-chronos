ca04158860f8159982d617ffec911ebeab77406c
Ekansh Agrawal <agrawalekansh@gmail.com>
2022-09-28 22:51:56 -0700

    rudimentary loss calc with streaming arch

--- src/chronos/chronos.py
+++ src/chronos/chronos.py
@@ -1,6 +1,5 @@
-import numpy as np
 from math import log
-import types
+import types, time
 
 # lookup table for the trajectories and their derivatives
 FXS = {
@@ -12,14 +11,56 @@ FXS = {
   "$2^x$": (lambda x: 2**x, lambda x: log(2)*2**x, lambda x: log(2)**2*2**x)
 }
 
+# num of iterations before we can potentially stop early
+EARLY_STOP = 5
+
 
 # return 2nd degree taylor expansion of f(x) at x0
 def taylor_expansion(x0, y, f1, f2):
   return lambda x: y + f1(x0)*(x-x0) + f2(x0)*(x-x0)**2/2
 
 # main timer function 
-def timer(func: types.FunctionType, mode: str, inp) -> str:
-  pass
+def timer(func: types.FunctionType, inp) -> str:
+  # init variables for storage
+  x = list(range(1, 50))
+  y = []
+  loss = {k: [0] for k in FXS.keys()}
+  arg_min = []
+
+  for i in x:
+    # record new measure for runtime
+    start = time.timer()
+    func(i)
+    elapsed = time.timer() - start
+    y.append(elapsed)
+
+    # no need to calculate loss for the first iteration
+    if i == 0:
+      continue
+    else:
+      for k, v in FXS.items():
+        # calculate taylor expansion at given point
+        f = taylor_expansion(i, y[i-1], v[1], v[2])
+        yhat = f(i)
+        # calculate loss for the current iteration
+        # and add it to runing sum
+        loss[k].append(abs(y - yhat) + loss[k][i-1])
+
+      # caculate argmin for the current iteration
+      arg_min.append(min(loss, key=lambda x: loss[x][-1]))
+
+
+    # no need to stop early if less than EARLY_STOP iterations
+    if len(arg_min) < EARLY_STOP:
+      continue
+    else:
+      # check if the last EARLY_STOP iterations have the same argmin
+      if all([arg_min[-1] == arg_min[-i] for i in range(1, EARLY_STOP)]):
+        return arg_min[-1]
+
+  # if we get here, we have to return the argmin of the last iteration
+  return arg_min[-1]
+
 
 
 # empty function for testing functionality
