dafae2d4f48692cd90bf7166c4ec5f9d00245ba2
Ekansh Agrawal <agrawalekansh@gmail.com>
2022-09-28 22:01:10 -0700

    added trajectories and taylor exp code

--- src/chronos/chronos.py
+++ src/chronos/chronos.py
@@ -1,2 +1,24 @@
+import numpy as np
+from math import log
+
+# lookup table for the trajectories and their derivatives
+FXS = {
+  "$x$": (lambda x: x, lambda x: 1, lambda x: 0),
+  "$x^2$": (lambda x: x**2, lambda x: 2*x, lambda x: 2),
+  "$x^3$": (lambda x: x**3, lambda x: 3*x**2, lambda x: 6*x),
+  "$log(x)$": (lambda x: log(x), lambda x: 1/x, lambda x: -1/x**2),
+  "$xlog(x)$": (lambda x: x*log(x), lambda x: log(x) + 1, lambda x: 1/x),
+  "$2^x$": (lambda x: 2**x, lambda x: log(2)*2**x, lambda x: log(2)**2*2**x)
+}
+
+
+# return 2nd degree taylor expansion of f(x) at x0
+def taylor_expansion(x0, y, f1, f2):
+  return lambda x: y + f1(x0)*(x-x0) + f2(x0)*(x-x0)**2/2
+
+# main timer function 
+def timer()
+
+# empty function for testing functionality
 def test():
   return "Hello World"
