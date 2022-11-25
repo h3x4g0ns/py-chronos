c8ad837c7659be563baf359ff321973a659fc179
Ekansh Agrawal <agrawalekansh@gmail.com>
2022-11-24 20:13:03 -0800

    mods in samples

--- examples/sample.py
+++ examples/sample.py
@@ -4,16 +4,25 @@ import chronos
 print(chronos.test())
 
 # test timer function
-def fib_linear(n):
+def fib_lin(n):
   a, b = 0, 1
   for i in range(n):
     a, b = b, a+b
   return a
 
-def fib_vannila(n):
+def fib_exp(n):
   if n <= 1:
     return n
-  return fib_vannila(n-1) + fib_vannila(n-2)
+  return fib_exp(n-1) + fib_exp(n-2)
 
-# print(chronos.timer(fib_linear))
-print(chronos.timer(fib_vannila))
+def fib_const(n):
+  dp = {i:fib_lin(i) for i in range(100)}
+  return dp[n]
+
+print("running linear time function")
+func, coeff = chronos.timer(fib_lin, silent=True, num=100)
+print(func, coeff, "\n")
+
+print("running constant time function")
+func, coeff = chronos.timer(fib_const, silent=True, num=50)
+print(func, coeff)
